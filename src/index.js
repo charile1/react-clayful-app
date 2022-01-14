d267f27f5f0f0f949ce8a2ea623119cb61c068a5
Kyungsuh Park <pyo05360@gmail.com>
2022-01-15 02:10:42 +0900

    ✨ 회원가입 UI 구현

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import clayful from "clayful/client-js"
 import axios from "axios";
+import "./css/auth.css";
 
 
 clayful.config({
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function RegisterPage() {
     return (
-        <div>
-            RegisterPage
+        <div className='auth-wrapper'>
+            <h1>회원가입</h1>
+            <form>
+                <input placeholder='Apple ID' type="email" name="email" defaultValue=""></input>
+                <input placeholder='암호' type="password" name="password" defaultValue=""></input>
+                <button type='submit' className='btn-register'>회원가입</button>
+            </form>
+           <Link to="login" style={{ color: "gray", textDecoration: "none" }}>{""}이미 Apple ID가 있다면? 지금 로그인.</Link>
         </div>
     )
 }
