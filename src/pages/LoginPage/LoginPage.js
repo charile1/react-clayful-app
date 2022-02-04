d98e25ac43dae379a2c069f5c514e19642c1eb5c
Kyungsuh Park <pyo05360@gmail.com>
2022-02-04 20:13:08 +0900

    회원가입, 로그인 구현

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import clayful from 'clayful/client-js'
 
 function LoginPage() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var Customer = clayful.Customer;
+
+        var payload = {
+            email,
+            password
+        };
+
+        Customer.authenticate(payload, function(err, result) {
+
+            if (err) {
+                // Error case
+                console.log(err.code);
+                return; //return이 있어야 아래로 내려가지 않고 멈춘다.
+            }
+
+            var data = result.data;
+            localStorage.setItem("customerUid", data.customer);
+            localStorage.setItem("accessToken", data.token);
+
+            // console.log(data);
+            
+            navigate("/");
+        });
+    }
+
     return (
-        <div>
-            LoginPage
-        </div>
+        <div className='auth-wrapper'>
+        <h1>로그인.</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+            onChange={handleEmailChange}
+            placeholder='Apple ID' 
+            type="email" 
+            name="email" 
+            value={email}
+            />
+            <input 
+            onChange={handlePasswordChange}
+            placeholder='암호' 
+            type="password" 
+            name="password" 
+            value={password}
+            />
+            <p>
+                Apple ID는 아이튠즈, 앱스토어, 아이클라우드에 로그인할 때 필요하니 꼭 만들어주세요요~!
+            </p>
+
+            <button type='submit' className='btn-register'>로그인</button>
+            <Link to="register" style={{ color: "gray", textDecoration: "none" }}>
+                {""}
+                Apple ID가 없으신가요? 지금 생성.
+           </Link>
+        </form>
+    </div>
     )
 }
 
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,16 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import clayful from 'clayful/client-js'
 
 function RegisterPage() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // input이 onchange할때마다 handleEmailChange 함수 발생 
+    const handleEmailChange = (event) => {
+        // console.log("event.target.value 아이디?", event.target.value);
+        setEmail(event.target.value);
+    }
+
+    // input이 onchange할때마다 handlePasswordChange 함수 발생 
+    const handlePasswordChange = (event) => {
+        // console.log("event.target.value 비번?", event.target.value);
+        setPassword(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); //페이지가 리프레쉬될 때 state가 초기화되는 것을 막아준다.
+        
+        var Customer = clayful.Customer;
+
+        var payload = {
+            email ,
+            password 
+        };
+        console.log("payload",payload);
+
+        // 클레이풀에 요청을 보내는 부분 
+        Customer.createMe(payload, function(err, result) {
+
+            if (err) {
+                // Error case
+                console.log(err.code);
+                return;
+            }
+            navigate("/login");
+
+            // var data = result.data;
+            // console.log(data);
+        });
+    }
+
     return (
         <div className='auth-wrapper'>
             <h1>회원가입</h1>
-            <form>
-                <input placeholder='Apple ID' type="email" name="email" defaultValue=""></input>
-                <input placeholder='암호' type="password" name="password" defaultValue=""></input>
+            <form onSubmit={handleSubmit}>
+                <input 
+                onChange={handleEmailChange}
+                placeholder='Apple ID' 
+                type="email" 
+                name="email" 
+                value={email}
+                />
+                <input 
+                onChange={handlePasswordChange}
+                placeholder='암호' 
+                type="password" 
+                name="password" 
+                value={password}
+                />
+
                 <button type='submit' className='btn-register'>회원가입</button>
+            <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
+               {""}
+               이미 Apple ID가 있다면? 지금 로그인.
+            </Link>
             </form>
-           <Link to="login" style={{ color: "gray", textDecoration: "none" }}>{""}이미 Apple ID가 있다면? 지금 로그인.</Link>
         </div>
     )
 }
